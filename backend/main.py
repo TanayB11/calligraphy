e800871c4bc15c47f2e65e91f6fdc305a662a8e5
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2024-03-11 16:21:08 -0700

    Reformat main.py

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,16 @@
-from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from llama_cpp import Llama
 from openai import OpenAI
 from dotenv import load_dotenv
-import json
+import os
 
 load_dotenv()
 
-openai_client = OpenAI()
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 llm = Llama(
-    model_path='../ml/models/mistral_similes-Q5_K_S.gguf',
+    model_path="../ml/models/mistral_similes-Q5_K_S.gguf",
     chat_format="mistral-instruct",
     n_ctx=8192,
     n_gpu_layers=-1,
@@ -95,7 +94,7 @@ async def gen_explode(prompt: Prompt):
 @app.post("/ml/critique_local/")
 async def gen_critique_local(prompt: Prompt):
     # https://www.reddit.com/r/ChatGPTPro/comments/14f6jqu/advanced_cot_prompts_for_gpt4_critical_ethical/
-    pre_prompt = '''
+    pre_prompt = """
         You are an advanced AI Language Model trained in persuasive writing, expository writing, and storytelling.
         Your task is to critique writing to the best of your ability.
         To ensure engaging and imaginative feedback, consider various writing elements and follow a step-by-step process.
@@ -114,7 +113,7 @@ async def gen_critique_local(prompt: Prompt):
             Point out which parts might need additional sources or fact-checking.
 
         Using this format, go ahead and create a well-crafted critique to the following writing:
-    '''
+    """
     completion = llm.create_completion(
         pre_prompt + prompt.prompt, max_tokens=4096, temperature=0.5
     )
@@ -124,7 +123,7 @@ async def gen_critique_local(prompt: Prompt):
 @app.post("/ml/critique_openai/")
 async def gen_critique_openai(prompt: Prompt):
     # https://www.reddit.com/r/ChatGPTPro/comments/14f6jqu/advanced_cot_prompts_for_gpt4_critical_ethical/
-    pre_prompt = '''
+    pre_prompt = """
         You are an advanced AI Language Model trained in persuasive writing, expository writing, and storytelling.
         Your task is to critique writing to the best of your ability.
         To ensure engaging and imaginative feedback, consider various writing elements and follow a step-by-step process.
@@ -169,18 +168,17 @@ async def gen_critique_openai(prompt: Prompt):
             }
           ]
         }
-    '''
+    """
 
     response = openai_client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
-        response_format={ "type": "json_object" },
+        response_format={"type": "json_object"},
         messages=[
             {"role": "system", "content": pre_prompt},
-            {"role": "user", "content": prompt.prompt}
+            {"role": "user", "content": prompt.prompt},
         ],
         temperature=0.2,
         seed=1337,
     )
 
-    return { 'prompt': prompt.prompt, 'completion': response.choices[0].message.content }
-
+    return {"prompt": prompt.prompt, "completion": response.choices[0].message.content}
